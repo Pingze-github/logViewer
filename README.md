2371fa9fbf76892cef9e6a3be2307f93d96c5e60
wangshunzhang <wang719695@gmail.com>
2018-09-16 17:25:49 +0800

    补充文档

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # logViewer
 日志阅读器
+
+## 依赖
+
++ github.com/gin-gonic/gin
++ github.com/Pingze-github/sift
+
+## Web框架
+
++ main.go 入口文件。初始化server
++ helper.go 辅助组件。为代码编写、服务返回提供支持
++ routes.go 路由。
++ handlers.go 控制器。
++ tools.go 功能函数集。
++ utils.go 通用工具函数集。
++ views/ 视图模板。
++ public/ 静态资源。
---
+++ servers/web.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func server(mode string, port int64) {
+	gin.SetMode(mode)
+
+	//获得路由实例
+	router := gin.Default()
+
+	// 静态资源
+	// router.StaticFile("/", "./public/index.html")
+
+	// 注册接口
+	handleRoutes(router)
+
+	// 启动服务
+	fmt.Println(`Server start @`, port)
+	router.Run(fmt.Sprintf(":%d", port))
+}
